c4a27608b5fe8ee0bad756324cb463d8fd8a00e0
baheersbi <baseer.baheer@berlinsbi.com>
2024-06-02 22:51:12 +0200

    New Updates

--- schema.sql
+++ schema.sql
@@ -1,10 +1,17 @@
-create database penpulse;
 
 use penpulse;
 
-CREATE TABLE Stores (
-    StoreID INT AUTO_INCREMENT PRIMARY KEY,
-    StoreName VARCHAR(255) NOT NULL,
-    Location VARCHAR(255) NOT NULL,
-    Contact VARCHAR(255)
-);
+CREATE TABLE Employees (
+                           EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
+                           SSN CHAR(11) NOT NULL UNIQUE,
+                           Name VARCHAR(255) NOT NULL,
+                           Gender ENUM('M', 'F', 'O') NOT NULL,
+                           DoB DATE NOT NULL,
+                           Position VARCHAR(255),
+                           Salary DECIMAL(10, 2),
+                           AffiliateStore INT,
+                           SupervisorID INT,
+                           FOREIGN KEY (AffiliateStore) REFERENCES Stores(StoreID),
+                           FOREIGN KEY (SupervisorID) REFERENCES Employees(EmployeeID)
+) ENGINE=INNODB;
+
